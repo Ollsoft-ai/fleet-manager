576343b8d52c86bcdb81c9e80313f5dbc11f2952
go98tad <go98tad@tum.de>
2024-11-23 21:13:48 +0100

    Ordered node by weight and added tree creation by Vehicles

--- Car assigning algorithm/Initial tree gen/TreeNode.cpp
+++ Car assigning algorithm/Initial tree gen/TreeNode.cpp
@@ -1,8 +1,18 @@
 #include "TreeNode.h"
+#include <algorithm>
 
-TreeNode::TreeNode(const std::string& nodeName) : name(nodeName) {}
+TreeNode::TreeNode(const std::string& nodeName, const float Nodeweight) 
+    : name(nodeName), weight(Nodeweight) {}
 
 void TreeNode::addChild(std::shared_ptr<TreeNode> child) {
-    child->parent = shared_from_this();
-    children.push_back(child);
+    child->parent = shared_from_this();  // Change to weak_from_this since parent is weak_ptr
+    
+    // Find the position to insert based on weight (descending order)
+    auto insertPos = std::lower_bound(children.begin(), children.end(), child,
+        [](const std::shared_ptr<TreeNode>& a, const std::shared_ptr<TreeNode>& b) {
+            return a->weight > b->weight;  // '>' for descending order
+        });
+    
+    // Insert at the found position
+    children.insert(insertPos, child);
 }
